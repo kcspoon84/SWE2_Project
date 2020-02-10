8cb2f54b1ada1a48aeaa07281589488a2c20e84f
DylanStuart <53539615+DylanStuart@users.noreply.github.com>
2020-02-10 15:29:19 -0600

    Add files via upload
    Added a bare bones hand class.

---
+++ Hand.java
@@ -0,0 +1,36 @@
+
+package SWE2_Project;
+import java.util.*;
+
+
+public class Hand {
+    
+    Card[] Delt = new Card[10];
+    
+   
+    
+    // Deals two cards out to the player or dealer
+   public void DealHand(Deck A){
+       for(int i = 0; i < 2; i++){
+       
+      Delt[i]= A.pop();
+      
+       
+       
+   }
+   }
+    // testing to see if i moved cards from deck to hand correctly
+    public void PrintHand(){
+       
+          for(int j = 0; j < 2; j++){
+              System.out.println(Delt[j].getSuit());
+              System.out.println(Delt[j].getNumber());
+              
+              
+          }
+            
+        }
+    
+    
+    
+}
